8de813d377ba434b100dedc9d4372cdd88c38d33
Ayush Bali <rishu.baliab@gmail.com>
2026-03-24 11:14:03 +0100

    user models

---
+++ models/user.models.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [3, "Name must be at least 3 characters long"],
+      maxlength: [50, "Name must be at most 50 characters long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: [/\S+@\S+\.S+/, "Please provide a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    subscriptions: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subscription",
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);

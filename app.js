722521c93377f5a4e51c72aa4b9aa9cf30e75c91
Ayush Bali <rishu.baliab@gmail.com>
2026-03-24 11:12:46 +0100

    mounted routes

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { PORT } from "./config/env.js";
+import { connectDB } from "./database/db.js";
+
+// IMPORT ROUTES
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+
+// USE ROUTES
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
+});
+
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
+app.use("/subscriptions", subscriptionRouter);
+
+// START SERVER
+app.listen(PORT, async () => {
+  console.log(
+    `Subscription tracker API is running on http://localhost:${PORT}`,
+  );
+  await connectDB();
+});
+
+export default app;

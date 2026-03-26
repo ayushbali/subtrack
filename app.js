ed67990eeba47ed0463830750e4968f109d3f4c1
Ayush Bali <rishu.baliab@gmail.com>
2026-03-26 12:32:46 +0100

    Added error handlers and middlewares

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import { connectDB } from "./database/db.js";
-import errorMiddleware from "./middleware/error.middleware.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 // IMPORT ROUTES
 import authRouter from "./routes/auth.routes.js";
@@ -16,22 +16,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 // USE ROUTES
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Subscription Tracker API!");
-});
-
-app.use("/auth", authRouter);
-app.use("/users", userRouter);
-app.use("/subscriptions", subscriptionRouter);
-
 // ERROR HANDLING MIDDLEWARE
 app.use(errorMiddleware);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
+});
 // START SERVER
 app.listen(PORT, async () => {
   console.log(
